c1abe61b57e9a6a219114262ac4df8f337cfbdc2
vaib-deloitte <101085470+vaib-deloitte@users.noreply.github.com>
2022-03-30 20:18:07 +0530

    Add files via upload

---
+++ error_handling.py
@@ -0,0 +1,35 @@
+class FormulaError(Exception): pass
+def parse_input(user_input):
+
+  input_list = user_input.split()
+  if len(input_list) != 3:
+    raise FormulaError('Input does not consist of three elements')
+  n1, op, n2 = input_list
+  try:
+    n1 = float(n1)
+    n2 = float(n2)
+  except ValueError:
+    raise FormulaError('The first and third input value must be numbers')
+  return n1, op, n2
+
+
+def calculate(n1, op, n2):
+
+  if op == '+':
+    return n1 + n2
+  if op == '-':
+    return n1 - n2
+  if op == '*':
+    return n1 * n2
+  if op == '/':
+    return n1 / n2
+  raise FormulaError('{0} is not a valid operator'.format(op))
+
+
+while True:
+  user_input = input('>>> ')
+  if user_input == 'quit':
+    break
+  n1, op, n2 = parse_input(user_input)
+  result = calculate(n1, op, n2)
+  print(result)
